fed47e26c036a4ec75a4d341aaf54d8cb6c0ac50
rhuangabrielsantos <rhuan.santos@db1.com.br>
2020-11-26 10:50:29 -0300

    Created project :tada:

---
+++ index.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+const config = require("./config.json");
+
+const { getCommandByDiscordMessage, getRandomThought } = require('./handler');
+
+const client = new Discord.Client();
+
+client.on("message", (message) => {
+    if (message.author.not) return;
+    if (!message.content.startsWith(config.PREFIX)) return;
+
+    const command = getCommandByDiscordMessage(message)
+
+    if (command == 'pensamento') {
+        let randomThought = getRandomThought();
+        message.reply(randomThought.message);
+    }
+})
+
+client.login(config.BOT_TOKEN);
