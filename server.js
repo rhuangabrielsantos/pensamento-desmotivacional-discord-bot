bb41e82ee4b9602d276dc49ef1de396b07755dcd
Rhuan Gabriel Santos <rhuan.santos@db1.com.br>
2021-07-20 18:42:21 -0300

    :fire: Remove files from old structure

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-let app = express();
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);
-});
